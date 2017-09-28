8fbfdaf4f141b7b5532c2bbd9bdf2981e24cbc28
Yoshinori Hirano <yoshinori.hirano@sonymobile.com>
2017-09-28 17:29:54 +0200

    Add Sound tile
    Add Sound tile to make user easier to change sound mode to manner mode.
    
    Sound Tile improvement (beanstown106 <nbenis106@gmail.com>)
    
    *uncoupled zen mode from sound tile let users use dnd for that
    *cycles through ring/vibrate/silent
    *added more descriptive labels for each state instead of always saying sound
    *added longpress action
    
    AICPfy:
    - Adapt to Nougat
    - Bring it to Oreo - @LorDClockaN
    - Bring it to Pie - @eyosen
    
    Change-Id: I9934e72758f162bd70ca578f6077fcd231fe7a6a

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -48,6 +48,7 @@ import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenRecordTile;
 import com.android.systemui.qs.tiles.UiModeNightTile;
 import com.android.systemui.qs.tiles.ScreenStabilizationTile;
+import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.qs.tiles.WorkModeTile;
@@ -201,6 +202,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mDataSwitchTileProvider.get();
             case "heads_up":
                 return mHeadsUpTileProvider.get();
+            case "sound":
+                return new SoundTile(mHost);
         }
 
         // Intent tiles.
